5c5e4990ae28e2b8b19a7ee4109ade5f1ae0a504
8i8 <iainardernhill@gmail.com>
2020-04-07 14:27:46 +0200

    init

---
+++ enc.go
@@ -0,0 +1,117 @@
+// enc requires the package github.com/atotto/clipboard
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/md5"
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/atotto/clipboard"
+)
+
+// MD5 Input flags.
+var MD5 = flag.Bool("md5", false, "Output a md5 hash.")
+
+// NEWLINE Input flags.
+var NEWLINE = flag.Bool("n", false, "Output the hash generated when a newline char is included in the string to be hashed.")
+
+// SHA256 Input flags.
+var SHA256 = flag.Bool("sha256", false, "Output a sha512 hash.")
+
+// SHA384 Input flags.
+var SHA384 = flag.Bool("sha384", false, "Output a sha384 hash.")
+
+// SHA512 Input flags.
+var SHA512 = flag.Bool("sha512", false, "Output a sha512 hash.")
+
+// CAPITAL capitalises the last letter in the sha for cases in which a capital
+// letter is a requirment.
+var CAPITAL = flag.Bool("c", false, "Capitalise the last letter in the hash.")
+
+func main() {
+
+	// Get flags from the os on program start.
+	flag.Parse()
+
+	for {
+		// Read stdin.
+		scanner := bufio.NewScanner(os.Stdin)
+
+		if !scanner.Scan() {
+			return
+		}
+
+		input := scanner.Bytes()
+		if len(input) == 0 {
+			return
+		}
+		if *NEWLINE {
+			input = append(input, '\n')
+		}
+
+		// Set the appropriate hash for the input, the given flag or lack there
+		// of, set the hashing algorithm. The resulting hash is copied to the system
+		// clipboard and then written to stdout.
+		byt := bytes.Buffer{}
+		if *MD5 {
+			x := md5.Sum(input)
+			byt.WriteString(fmt.Sprintf("%x", x))
+			b := caps(byt.Bytes())
+			clipboard.WriteAll(string(b))
+			fmt.Println(string(b))
+		} else if *SHA256 {
+			x := sha256.Sum256(input)
+			byt.WriteString(fmt.Sprintf("%x", x))
+			b := caps(byt.Bytes())
+			clipboard.WriteAll(string(b))
+			fmt.Println(string(b))
+		} else if *SHA384 {
+			x := sha512.Sum384(input)
+			byt.WriteString(fmt.Sprintf("%x", x))
+			b := caps(byt.Bytes())
+			clipboard.WriteAll(string(b))
+			fmt.Println(string(b))
+		} else if *SHA512 {
+			x := sha512.Sum512(input)
+			byt.WriteString(fmt.Sprintf("%x", x))
+			b := caps(byt.Bytes())
+			clipboard.WriteAll(string(b))
+			fmt.Println(string(b))
+		} else {
+			// Default hash is md5
+			x := md5.Sum(input)
+			byt.WriteString(fmt.Sprintf("%x", x))
+			b := caps(byt.Bytes())
+			clipboard.WriteAll(string(b))
+			fmt.Println(string(b))
+		}
+	}
+}
+
+func isLetter(c byte) bool {
+	if c > 96 && c < 123 {
+		return true
+	}
+	return false
+}
+
+func toUpper(c byte) byte {
+	return c - 32
+}
+
+func caps(b []byte) []byte {
+	if *CAPITAL {
+		for c := len(b) - 1; c > 0; c-- {
+			if isLetter(b[c]) {
+				b[c] = toUpper(b[c])
+				break
+			}
+		}
+	}
+	return b
+}

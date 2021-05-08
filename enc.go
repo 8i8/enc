ae60124a00ff46aea28210989c6fec4dbb113624
8i8 <iainardernhill@gmail.com>
2021-05-08 23:06:23 +0200

    updated for lib

--- enc.go
+++ enc.go
@@ -1,102 +1,106 @@
-// enc requires the package github.com/atotto/clipboard
-package main
+package enc
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/md5"
 	"crypto/sha256"
 	"crypto/sha512"
-	"flag"
 	"fmt"
-	"os"
-
-	"github.com/atotto/clipboard"
 )
 
-var (
+type Encoder interface {
+	Encode(string) string
+	Setup(...string)
+}
+
+func NewEncoder() Encoder {
+	return &settings{}
+}
+
+type settings struct {
 	// MD5 Input flags.
-	MD5 = flag.Bool("md5", false, "Output a md5 hash.")
+	MD5 bool
 	// NEWLINE Input flags.
-	NEWLINE = flag.Bool("n", false, "Output the hash generated when a newline char is included in the string to be hashed.")
+	NEWLINE bool
 	// SHA256 Input flags.
-	SHA256 = flag.Bool("sha256", false, "Output a sha512 hash.")
+	SHA256 bool
 	// SHA384 Input flags.
-	SHA384 = flag.Bool("sha384", false, "Output a sha384 hash.")
+	SHA384 bool
 	// SHA512 Input flags.
-	SHA512 = flag.Bool("sha512", false, "Output a sha512 hash.")
-	pp     = flag.Bool("pp", false, "")
-)
+	SHA512 bool
+	// pp truncates the output to 20 characters.
+	pp bool
+	// CAPITAL capitalises the last letter in the sha for cases in which a capital
+	// letter is a requirment.
+	CAPITAL bool
+}
 
-// CAPITAL capitalises the last letter in the sha for cases in which a capital
-// letter is a requirment.
-var CAPITAL = flag.Bool("c", false, "Capitalise the last letter in the hash.")
+func (s settings) Encode(str string) string {
 
-func main() {
+	var out string
+	input := []byte(str)
 
-	// Get flags from the os on program start.
-	flag.Parse()
+	// Set the appropriate hash for the input, the given flag or lack there
+	// of, set the hashing algorithm. The resulting hash is copied to the system
+	// clipboard and then written to stdout.
+	byt := bytes.Buffer{}
+	switch {
+	case s.MD5:
+		x := md5.Sum(input)
+		byt.WriteString(fmt.Sprintf("%x", x))
+		b := s.caps(byt.Bytes())
+		out = string(b)
+	case s.SHA256:
+		x := sha256.Sum256(input)
+		byt.WriteString(fmt.Sprintf("%x", x))
+		b := s.caps(byt.Bytes())
+		out = string(b)
+	case s.SHA384:
+		x := sha512.Sum384(input)
+		byt.WriteString(fmt.Sprintf("%x", x))
+		b := s.caps(byt.Bytes())
+		out = string(b)
+	case s.SHA512:
+		x := sha512.Sum512(input)
+		byt.WriteString(fmt.Sprintf("%x", x))
+		b := s.caps(byt.Bytes())
+		out = string(b)
+	case s.pp:
+		x := md5.Sum(input)
+		byt.WriteString(fmt.Sprintf("%x", x))
+		b := s.caps(byt.Bytes())
+		b = b[:20]
+		out = string(b)
+	default:
+		// Default hash is md5
+		x := md5.Sum(input)
+		byt.WriteString(fmt.Sprintf("%x", x))
+		b := s.caps(byt.Bytes())
+		out = string(b)
+	}
+	s = settings{} // reset all
+	return out
+}
 
-	for {
-		// Read stdin.
-		scanner := bufio.NewScanner(os.Stdin)
-
-		if !scanner.Scan() {
-			return
-		}
-
-		input := scanner.Bytes()
-		if len(input) == 0 {
-			return
-		}
-		if *NEWLINE {
-			input = append(input, '\n')
-		}
-
-		// Set the appropriate hash for the input, the given flag or lack there
-		// of, set the hashing algorithm. The resulting hash is copied to the system
-		// clipboard and then written to stdout.
-		byt := bytes.Buffer{}
-		switch {
-		case *MD5:
-			x := md5.Sum(input)
-			byt.WriteString(fmt.Sprintf("%x", x))
-			b := caps(byt.Bytes())
-			clipboard.WriteAll(string(b))
-			fmt.Println(string(b))
-		case *SHA256:
-			x := sha256.Sum256(input)
-			byt.WriteString(fmt.Sprintf("%x", x))
-			b := caps(byt.Bytes())
-			clipboard.WriteAll(string(b))
-			fmt.Println(string(b))
-		case *SHA384:
-			x := sha512.Sum384(input)
-			byt.WriteString(fmt.Sprintf("%x", x))
-			b := caps(byt.Bytes())
-			clipboard.WriteAll(string(b))
-			fmt.Println(string(b))
-		case *SHA512:
-			x := sha512.Sum512(input)
-			byt.WriteString(fmt.Sprintf("%x", x))
-			b := caps(byt.Bytes())
-			clipboard.WriteAll(string(b))
-			fmt.Println(string(b))
-		case *pp:
-			x := md5.Sum(input)
-			byt.WriteString(fmt.Sprintf("%x", x))
-			b := caps(byt.Bytes())
-			var b2 [20]byte
-			copy(b2[:], b)
-			clipboard.WriteAll(string(b2[0:20]))
-			fmt.Println(string(b2[0:20]))
+func (s *settings) Setup(args ...string) {
+	for _, str := range args {
+		switch str {
+		case "md5":
+			s.MD5 = true
+		case "newline":
+			s.NEWLINE = true
+		case "sha256":
+			s.SHA256 = true
+		case "sha384":
+			s.SHA384 = true
+		case "sha512":
+			s.SHA512 = true
+		case "pp":
+			s.pp = true
+		case "capital":
+			s.CAPITAL = true
 		default:
-			// Default hash is md5
-			x := md5.Sum(input)
-			byt.WriteString(fmt.Sprintf("%x", x))
-			b := caps(byt.Bytes())
-			clipboard.WriteAll(string(b))
-			fmt.Println(string(b))
+			Err(nil, "encoding", "Setup", "Unknown argument")
 		}
 	}
 }
@@ -112,8 +116,8 @@ func toUpper(c byte) byte {
 	return c - 32
 }
 
-func caps(b []byte) []byte {
-	if *CAPITAL {
+func (s settings) caps(b []byte) []byte {
+	if s.CAPITAL {
 		for c := len(b) - 1; c > 0; c-- {
 			if isLetter(b[c]) {
 				b[c] = toUpper(b[c])
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/8i8/lib/enc
+
+go 1.15
+
+require github.com/atotto/clipboard v0.1.2 // indirect
